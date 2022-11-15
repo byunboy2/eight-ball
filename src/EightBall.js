c27eb22f708d5dc022bdb0b9a650bef5aa0054d1
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-11-15 12:05:31 -0800

    Finish making eightball

---
+++ src/EightBall.js
@@ -0,0 +1,39 @@
+import { defaultAnswers } from "./answers.js"
+import _ from "underscore"
+import {useState} from "react"
+import "./EightBall.css"
+
+/** EightBall: Shows starting message "Think of a Question"
+ *  when clicked, show instead a random answer and change color.
+ *
+ *  Props:
+ *  - answers: array of {msg, color} objects
+ *
+ *  State:
+ *  - answer: {msg, color} of current answer
+ *
+ *  App -> EightBall
+ */
+
+function EightBall( {answers = defaultAnswers} ) {
+  const [answer, setAnswer] = useState({
+    msg: "Think of a Question.",
+    color: "black",
+  });
+
+  function handleClick(evt) {
+    const shuffledAnswers = _.shuffle(answers);
+    setAnswer(shuffledAnswers[0]);
+  }
+
+  return (
+    <div
+        className="EightBall"
+        onClick={handleClick}
+        style={{backgroundColor: answer.color}}>
+      <b className="EightBall-answer">{answer.msg}</b>
+    </div>
+  )
+}
+
+export default EightBall
